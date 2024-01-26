f2c7900b0f90dc062420e62cbdbfb9083927c201
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-26 16:05:21 -0500

    created FastAPI server

---
+++ src/server/app/__init__.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+# Create the App
+app = FastAPI()
+
+# Apply CORS Middleware / Allow Origins
+origins = [ 
+           'http://localhost:3000',
+           'http://localhost:3005',
+           'https://cher-ami.roberthogan.io',
+           'https://www.cher-ami.roberthogan.io' 
+        ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials= True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+# Start Up Events
+@app.on_event("startup")
+async def startup_event():
+    print("Starting Server...")
+    
+    
+# Root Render
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Hello! Welcome to World View Insights."}
