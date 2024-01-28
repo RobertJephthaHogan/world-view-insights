d95b85ef4eab1e097104b648f098ca86dd7647ea
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-28 15:03:44 -0500

    notable quotes UI

---
+++ src/client/src/components/NotableQuotes/index.tsx
@@ -0,0 +1,334 @@
+import React from 'react'
+import './styles.css'
+import CaretRightOutlined from '@ant-design/icons/CaretRightOutlined'
+
+
+
+export default function NotableQuotes() {
+
+    return (
+        <div className='notable-quotes'>
+            <div className='quote-section'>
+                <div className='quote-section-title-bar'>
+                    <span className='quote-section-title'>
+                        Market Leaders
+                    </span>
+                    <CaretRightOutlined
+                         className='quote-section-caret'
+                    />
+                </div>
+                <div className='section-quote-container'>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                META
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-chip'>
+                                <span className='quote-change-text'>
+                                    0%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $392.21
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='quote-section mts'>
+                <div className='quote-section-title-bar'>
+                    <span className='quote-section-title'>
+                        Gainers
+                    </span>
+                    <CaretRightOutlined
+                         className='quote-section-caret'
+                    />
+                </div>
+                <div className='section-quote-container'>
+                <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                GOOG
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-up-chip'>
+                                <span className='quote-change-up-text'>
+                                    +1.76%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='quote-section mts'>
+                <div className='quote-section-title-bar'>
+                    <span className='quote-section-title'>
+                        Losers
+                    </span>
+                    <CaretRightOutlined
+                         className='quote-section-caret'
+                    />
+                </div>
+                <div className='section-quote-container'>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                    <div className='quote-row'>
+                        <div className='qrl'>
+                            <span className='quote-row-symbol'>
+                                AAPL
+                            </span>
+                        </div>
+                        <div className='qrc'>
+                            <div className='quote-change-down-chip'>
+                                <span className='quote-change-down-text'>
+                                    -0.28%
+                                </span>
+                            </div>
+                        </div>
+                        <div className='qrr'>
+                            <span className='quote-row-quote'>
+                                $193.96
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
