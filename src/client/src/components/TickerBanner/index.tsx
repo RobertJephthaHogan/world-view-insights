464728cdc42e3609ed92601a3fd022623708fbfe
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-28 00:27:13 -0500

    cleanup

--- src/client/src/components/TickerBanner/index.tsx
+++ src/client/src/components/TickerBanner/index.tsx
@@ -6,14 +6,12 @@ import { dataService } from '../../services/data.service'
 
 
 
-// TODO: REFACTOR BANNER ITEMS WITH DYNAMIC COMPONENT
 export default function TickerBanner() {
 
     const [bannerData, setBannerData] = useState<Array<any>|null>(null)
 
 
     useEffect(() => {
-        //TODO: get banner data from backend and set to state
 
         console.log('hello')
         
@@ -90,169 +88,6 @@ export default function TickerBanner() {
                         )
                     })
                 }
-
-                {/* <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        AAPL - $193.55
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.28%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        MSFT - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        -0.18%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        NVDA - $612.25
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.32%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        WBA - $22.89
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        META - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        AAPL - $193.55
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.28%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        MSFT - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        -0.18%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        NVDA - $612.25
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.32%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        WBA - $22.89
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        META - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div> */}
-
-                {/* Set Two */}
-                {/* <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        AAPL - $193.55
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.28%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        MSFT - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        -0.18%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        NVDA - $612.25
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.32%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        WBA - $22.89
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        META - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        AAPL - $193.55
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.28%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        MSFT - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        -0.18%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        NVDA - $612.25
-                    </span>
-                    <span className='bi-change-down-text'>
-                        -0.32%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        WBA - $22.89
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div>
-                <div className='banner-item item'>
-                    <span className='bi-info-text'>
-                        META - $403.25
-                    </span>
-                    <span className='bi-change-up-text'>
-                        +0.14%
-                    </span>
-                </div> */}
 
             </div>
         </div>
