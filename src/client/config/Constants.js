a931ad54fd9a36f5528e0bce5e6c020b6ee14a56
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-31 16:29:51 -0500

    prod url

--- src/client/config/Constants.js
+++ src/client/config/Constants.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:8008/'
+// const API_BASE_URL = 'http://localhost:8008/'
+const API_BASE_URL = 'https://api.worldviewinsights.com/'
 
 
 export function getConfig() {
