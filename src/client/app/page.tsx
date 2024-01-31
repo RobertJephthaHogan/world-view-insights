11bbdc206dc0a0d8dcd527ea537552b478bf922a
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-30 20:05:02 -0500

    port header to next

--- src/client/app/page.tsx
+++ src/client/app/page.tsx
@@ -1,10 +1,34 @@
-import Image from "next/image";
-import styles from "./styles/page.module.css";
+import Header from '../components/Header'
+// import TickerBanner from '../../components/TickerBanner'
+// import IndexBanner from '../../components/IndexBanner'
+// import NotableQuotes from '../../components/NotableQuotes'
+// import NewsSection from '../../components/NewsSection'
+import styles from '../styles/page.module.css'
 
-export default function Home() {
-	return (
-		<main className={styles.main}>
-			Homepage
-		</main>
-	);
+
+
+export default function Homepage() {
+
+    return (
+        <div>
+            <Header/>
+            {/* <TickerBanner/>
+            <IndexBanner/>
+            <div className={styles['homepage-body']}>
+                <div className={styles['hpb-l']}>
+                    <div className={styles['hpb-l-title-container']}>
+                        <span className={styles['latest-news-title']}>
+                            Latest News
+                        </span>
+                    </div>
+                    <NewsSection/>
+                    <NewsSection/>
+                    <NewsSection/>
+                </div>
+                <div className={styles['hpb-r']}>z
+                    <NotableQuotes/>
+                </div>
+            </div> */}
+        </div>
+    )
 }
