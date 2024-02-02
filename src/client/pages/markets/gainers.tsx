e0ab07883949a8faacb6012557f79e5962c34b65
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-01 21:55:11 -0500

    meta, head, title setup fixes

--- src/client/pages/markets/gainers.tsx
+++ src/client/pages/markets/gainers.tsx
@@ -1,12 +1,17 @@
 import { TrackingProvider } from '@/providers/TrackingProvider'
 import React from 'react'
-
+import Head from 'next/head';
 
 
 
 export default function Gainers() {
     return (
         <TrackingProvider>
+            <Head>
+                <title>
+                    WorldView Insights | Gainers
+                </title>
+            </Head>
             <div>
                 Gainers 
             </div>
