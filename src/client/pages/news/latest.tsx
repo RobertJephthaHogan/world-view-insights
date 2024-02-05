2895407507a62a72dd28d4c1bd7c6b965166f5ef
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-05 18:18:56 -0500

    latest News Component

--- src/client/pages/news/latest.tsx
+++ src/client/pages/news/latest.tsx
@@ -1,6 +1,15 @@
 import { TrackingProvider } from '@/providers/TrackingProvider'
 import React from 'react'
 import Head from 'next/head';
+import styles from '../../styles/pages/news/latest.module.css'
+import Header from '@/components/Header';
+import TickerBanner from '@/components/TickerBanner';
+import IndexBanner from '@/components/IndexBanner';
+import { Inter } from "next/font/google";
+import BusinessNew from '@/components/news/BusinessNews';
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 
 
@@ -12,8 +21,39 @@ export default function LatestNews() {
                     Latest News | WorldView Insights
                 </title>
             </Head>
-            <div>
-                Latest News
+            <div className={inter.className}>
+                <Header/>
+                <TickerBanner/>
+                <IndexBanner/>
+                <div className={styles['latest-news-component']}>
+                    <div>
+
+                    </div>
+                    <div>
+                        <div className={styles['ln-title-row']}>
+                            <span className={styles['latest-news-title']}>
+                                Latest News
+                            </span>
+                        </div>
+                        <div className={styles['news-container']}>
+                            <BusinessNew
+                                page={1}
+                            />
+                            <BusinessNew
+                                page={2}
+                                sectionTitle={"More Business News"}
+                            />
+                            <BusinessNew
+                                page={3}
+                                sectionTitle={"More Business News"}
+                            />
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                
             </div>
         </TrackingProvider>
     )
