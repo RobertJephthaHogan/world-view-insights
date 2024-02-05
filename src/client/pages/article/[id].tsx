358f7b08705abd428ce66d7c9de88bbcfeea6840
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-05 00:45:01 -0500

    article UI

--- src/client/pages/article/[id].tsx
+++ src/client/pages/article/[id].tsx
@@ -6,6 +6,7 @@ import { TrackingProvider } from '@/providers/TrackingProvider';
 import { GetServerSideProps, NextPage } from 'next';
 import { Inter } from "next/font/google";
 import { newsService } from '@/services/news.service';
+import styles from '../../styles/pages/article.module.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +24,40 @@ const ArticlePage: NextPage<any> = ({ article }) => {
 			<Header/>
 			<TickerBanner/>
 			<IndexBanner/>
-			<h1>{article?.title}</h1>
-			<p>{article?.content}</p>
-			<p>Article Page!</p>
+			<div className={styles['article-component']}>
+				<div>
+
+				</div>
+				<div className={styles['ac-center']}>
+					<div className={styles['ticker-wrapper']}>
+						<span>
+							{article?.tickers}
+						</span>
+					</div>
+					<div className={styles['article-title-container']}>
+						<span className={styles['article-title']}>
+							{article?.title}
+						</span>
+					</div>
+					<div className={styles['article-info']}>
+						<span className={styles['article-info-text']}>
+							Published at {article?.datePosted}
+						</span>
+						<span className={styles['article-info-text']}>
+							by {article?.author}
+						</span>
+					</div>
+					<div>
+						<p className={styles['article-content']}>
+							{article?.content}
+						</p>
+					</div>
+				</div>
+				<div>
+
+				</div>
+			</div>
+			
 		</div>
 	</TrackingProvider>
   );
@@ -36,7 +68,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   
 	try {
 		const resp = await newsService.getArticleById(id); // Use await to wait for the promise to resolve
-		console.log('resp', resp);
 	
 		// Assuming resp directly contains the article data you want to pass as props
 		return {
