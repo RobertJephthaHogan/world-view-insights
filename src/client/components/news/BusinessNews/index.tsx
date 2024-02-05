47b5d4b8bb356e1e4b91d2fb49deadb2c25804d6
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-04 22:51:04 -0500

    Business News Section UI

---
+++ src/client/components/news/BusinessNews/index.tsx
@@ -0,0 +1,42 @@
+import NewsSection from '@/components/NewsSection'
+import React, { useEffect, useState } from 'react'
+import {newsService} from '@/services/news.service'
+import { paginateArray } from '@/utils/pagination'
+
+
+interface BusinessNewsProps {
+    page: number
+    sectionTitle?: any
+    moreNewsTitle?: any
+}
+
+export default function BusinessNew(props: BusinessNewsProps) {
+
+    const [articleSet, setArticleSet] = useState<any>([])
+
+
+    useEffect(() => {
+
+        newsService.getBusinessNewsArticles(20)
+            .then((resp: any) => {
+                console.log('resp', resp)
+                const thisSet = paginateArray(resp, props.page, 4)
+                setArticleSet(thisSet)
+            })
+            .catch((error: any) => {
+                console.error('Error Getting Business News Articles', error)
+            })
+
+
+    }, [props.page])
+
+    return (
+        <div>
+            <NewsSection
+                sectionTitle={props.sectionTitle ? props.sectionTitle : "Business News"}
+                moreNewsTitle={props.moreNewsTitle ? props.moreNewsTitle : "More Business News"}
+                articles={articleSet}
+            />
+        </div>
+    )
+}
