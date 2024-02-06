8dc7ed3a7b3afc3b7363f26ccc7838f34e3fb389
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-06 18:04:18 -0500

    coming soon

--- src/client/pages/markets/currencies.tsx
+++ src/client/pages/markets/currencies.tsx
@@ -1,7 +1,13 @@
 import { TrackingProvider } from '@/providers/TrackingProvider'
 import React from 'react'
 import Head from 'next/head';
+import Header from '@/components/Header';
+import { Inter } from "next/font/google";
+import styles from '../../styles/pages/markets/currencies.module.css'
+import ComingSoon from '@/components/ComingSoon';
 
+
+const inter = Inter({ subsets: ["latin"] });
 
 
 
@@ -12,9 +18,24 @@ export default function Currencies() {
                 <title>
                     Currencies | WorldView Insights
                 </title>
+                <meta
+                    name="description"
+                    content="Global Currencies Data - WorldView Insights"
+                />
             </Head>
-            <div>
-                Currencies Overview
+            <div className={inter.className}>
+                <Header/>
+                <div className={styles['currencies-component']}>
+                    <div>
+
+                    </div>
+                    <div className={styles['cc-center']}>
+                        <ComingSoon/>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
         </TrackingProvider>
     )
--- src/client/pages/markets/sectors.tsx
+++ src/client/pages/markets/sectors.tsx
@@ -1,7 +1,13 @@
 import { TrackingProvider } from '@/providers/TrackingProvider'
 import React from 'react'
 import Head from 'next/head';
+import Header from '@/components/Header';
+import { Inter } from "next/font/google";
+import styles from '../../styles/pages/markets/sectors.module.css'
+import ComingSoon from '@/components/ComingSoon';
 
+
+const inter = Inter({ subsets: ["latin"] });
 
 
 
@@ -12,9 +18,24 @@ export default function Sectors() {
                 <title>
                     Market Sectors | WorldView Insights
                 </title>
+                <meta
+                    name="description"
+                    content="United Stated Stock Market Sectors Overview - WorldView Insights"
+                />
             </Head>
-            <div>
-                Sectors 
+            <div className={inter.className}>
+                <Header/>
+                <div className={styles['sectors-component']}>
+                    <div>
+
+                    </div>
+                    <div className={styles['sc-center']}>
+                        <ComingSoon/>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
         </TrackingProvider>
     )
