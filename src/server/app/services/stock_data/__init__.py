14bb5cf16498f1e8d43c38b2b6557b349ea6286b
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-07 20:09:32 -0500

    get company outlook

--- src/server/app/services/stock_data/__init__.py
+++ src/server/app/services/stock_data/__init__.py
@@ -2,11 +2,17 @@
 
 
 
+from app.services.fmp import FmpService
+
+
 class StockDataService:
     
     
     async def get_stock_page_data(ticker):
         
-        return {}
+        company_data = await FmpService.CompanyData.get_company_outlook(ticker)
+        print('company_data', company_data.json())
+        
+        return company_data.json()
     
     
