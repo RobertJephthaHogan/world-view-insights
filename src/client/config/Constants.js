0789b73783995fee5e3eeb0154258df1fe9d5922
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-12 21:11:34 -0500

    url

--- src/client/config/Constants.js
+++ src/client/config/Constants.js
@@ -1,8 +1,8 @@
 
 
 
-const API_BASE_URL = 'http://127.0.0.1:8008/'
-//const API_BASE_URL = 'https://api.worldviewinsights.com/'
+//const API_BASE_URL = 'http://127.0.0.1:8008/'
+const API_BASE_URL = 'https://api.worldviewinsights.com/'
 
 // const CLIENT_BASE_URL = 'http://localhost:3008/'
 const CLIENT_BASE_URL = 'https://worldviewinsights.com/'
