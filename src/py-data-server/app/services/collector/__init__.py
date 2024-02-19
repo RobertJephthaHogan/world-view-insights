c0a7ed10ccbb2a154fd265656f2e2a04e932c73b
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-19 18:48:32 -0500

    cleanup

--- src/py-data-server/app/services/collector/__init__.py
+++ src/py-data-server/app/services/collector/__init__.py
@@ -16,40 +16,29 @@ class CollectorService:
         alreadyInDB = 0
         newFilings = 0
         recentFilings = await EdgarService.getRecentFilings("4", "", "")
-        
-        print('recentFilings', recentFilings)
-        
+                
         for filing in recentFilings:
-            
-            print('filing', filing)
-            
-            # check if the filing exists in the database, if it exists, do nothing
+                        
+            # check if the filing exists in the database, 
             filing_exists = await FormFourOperations.retrieve_form_four_by_accession_id(filing)
             
+            # if the filing already exists, do nothing
             if (len(filing_exists)):
-                # do not collect the existing filing
                 pass
             
+            # if the filing does not already exist, create the entry
             if not (len(filing_exists)):
-                # if the filing does not already exist, create the entry
-                print('Collecting New Form Four Filing...')
-                
+                                
                 newFiling = filing
                 soup, filing = await EdgarService.getFiling(newFiling)
                 formFourDTO = parseFormFour(soup, filing)
                 formFourDTO['id'] = str(ObjectId())
-
-                # print('newFiling', newFiling)
-                # print('soup', soup)
-                # print('filing', filing)
-                print('formFourDTO', formFourDTO)
                 
                 form_four_obj = FormFour(**formFourDTO)
                 await FormFourOperations.add_form_four(form_four_obj)
                 
                 
-                pass
                 
-            print('filing_exists?', filing_exists)
+                
         
-        pass
+        
