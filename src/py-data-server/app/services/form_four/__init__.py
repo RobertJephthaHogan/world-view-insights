acf1a30b09aa37e3cbc4527f55476d1df854e541
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-02-26 23:03:41 -0500

    form four parser latest

--- src/py-data-server/app/services/form_four/__init__.py
+++ src/py-data-server/app/services/form_four/__init__.py
@@ -32,7 +32,7 @@ class FormFourService:
     
     
     def fix_string_zero_value(self, value):
-        
+        """If transaction share or execution prices are blank, autofill with 0"""
         try:
             value = float(value)
         except ValueError:
@@ -48,19 +48,22 @@ class FormFourService:
         non_derivative_table_dict = {"nonDerivativeTransactions": {}, "nonDerivativeHoldings": {}}
         derivative_table_dict = {"derivativeTransactions": {}, "derivativeHoldings": {}}
         
-        # Function to process individual transactions or holdings
+        # Function to process entries in the transactions or holdings tables
         def process_entries(entries, entry_fields, entry_type):
             entry_dict = {}
             for counter, entry in enumerate(entries, 1):
+                
+                # Extract the fields from the tables
                 fields = {field: self.extract_form_four_field(entry.find(field)) for field in entry_fields}
-                # Special handling for fields requiring transformation
+                
+                # Special handling for fields requiring transformation tp calc transaction size
                 transaction_shares = entry.find('transactionShares')
                 transaction_shares = self.fix_string_zero_value(self.extract_form_four_field(transaction_shares, default="0"))
-                
                 transaction_price_per_share = entry.find('transactionPricePerShare')
                 transaction_price_per_share = self.fix_string_zero_value(self.extract_form_four_field(transaction_price_per_share, default="0"))
-                
                 fields['transactionSize'] = float(transaction_shares) * float(transaction_price_per_share)
+                
+                # set the entry key and value
                 entry_dict[f"{entry_type}_{counter}"] = fields
 
             return entry_dict
@@ -71,14 +74,33 @@ class FormFourService:
             non_derivative_transactions = non_derivative_table.find_all('nonDerivativeTransaction')
             non_derivative_table_dict["nonDerivativeTransactions"] = process_entries(
                                                                 non_derivative_transactions, 
-                                                                ['securityTitle', 'transactionDate', 'deemedExecutionDate', 'transactionFormType', 'transactionCode', 'equitySwapInvolved', 'transactionShares', 'transactionPricePerShare', 'transactionAcquiredDisposedCode', 'sharesOwnedFollowingTransaction', 'directOrIndirectOwnership'],
+                                                                [
+                                                                    # Non-derivative transaction fields
+                                                                    'securityTitle', 
+                                                                    'transactionDate', 
+                                                                    'deemedExecutionDate', 
+                                                                    'transactionFormType', 
+                                                                    'transactionCode', 
+                                                                    'equitySwapInvolved', 
+                                                                    'transactionShares', 
+                                                                    'transactionPricePerShare', 
+                                                                    'transactionAcquiredDisposedCode', 
+                                                                    'sharesOwnedFollowingTransaction', 
+                                                                    'directOrIndirectOwnership'
+                                                                ],
                                                                 'nonDerivativeTransaction'
                                                                 )
 
             non_derivative_holdings = non_derivative_table.find_all('nonDerivativeHolding')
             non_derivative_table_dict["nonDerivativeHoldings"] = process_entries(
                                                                 non_derivative_holdings, 
-                                                                ['securityTitle', 'sharesOwnedFollowingTransaction', 'directOrIndirectOwnership', 'natureOfOwnership'],
+                                                                [
+                                                                    # Non-derivative holding fields
+                                                                    'securityTitle', 
+                                                                    'sharesOwnedFollowingTransaction', 
+                                                                    'directOrIndirectOwnership', 
+                                                                    'natureOfOwnership'
+                                                                 ],
                                                                 'nonDerivativeHolding'
                                                                 )
 
@@ -88,14 +110,44 @@ class FormFourService:
             derivative_transactions = derivative_table.find_all('derivativeTransaction')
             derivative_table_dict["derivativeTransactions"] = process_entries(
                                                             derivative_transactions, 
-                                                            ['securityTitle', 'conversionOrExercisePrice', 'transactionDate', 'deemedExecutionDate', 'transactionFormType', 'transactionCode', 'equitySwapsInvolved', 'transactionTimeliness', 'transactionShares', 'transactionPricePerShare', 'transactionAcquiredDisposedCode', 'exerciseDate', 'expirationDate', 'underlyingSecurityTitle', 'underlyingSecurityShares', 'sharesOwnedFollowingTransaction', 'directOrIndirectOwnership'],
+                                                            [
+                                                                # derivative transaction fields
+                                                                'securityTitle', 
+                                                                'conversionOrExercisePrice', 
+                                                                'transactionDate', 
+                                                                'deemedExecutionDate', 
+                                                                'transactionFormType', 
+                                                                'transactionCode', 
+                                                                'equitySwapsInvolved', 
+                                                                'transactionTimeliness', 
+                                                                'transactionShares', 
+                                                                'transactionPricePerShare', 
+                                                                'transactionAcquiredDisposedCode', 
+                                                                'exerciseDate', 
+                                                                'expirationDate', 
+                                                                'underlyingSecurityTitle', 
+                                                                'underlyingSecurityShares', 
+                                                                'sharesOwnedFollowingTransaction', 
+                                                                'directOrIndirectOwnership'
+                                                            ],
                                                             'derivativeTransaction'
                                                             )
 
             derivative_holdings = derivative_table.find_all('derivativeHolding')
             derivative_table_dict["derivativeHoldings"] = process_entries(
                                                         derivative_holdings, 
-                                                        ['securityTitle', 'conversionOrExercisePrice', 'exerciseDate', 'expirationDate', 'underlyingSecurityTitle', 'underlyingSecurityShares', 'sharesOwnedFollowingTransaction', 'valueOwnedFollowingTransaction', 'directOrIndirectOwnership'],
+                                                        [
+                                                            # derivative holding fields
+                                                            'securityTitle', 
+                                                            'conversionOrExercisePrice', 
+                                                            'exerciseDate', 
+                                                            'expirationDate', 
+                                                            'underlyingSecurityTitle', 
+                                                            'underlyingSecurityShares', 
+                                                            'sharesOwnedFollowingTransaction', 
+                                                            'valueOwnedFollowingTransaction', 
+                                                            'directOrIndirectOwnership'
+                                                        ],
                                                         'derivativeHolding'
                                                         )
 
@@ -113,10 +165,10 @@ class FormFourService:
             "issuerMarketCap": str(DFS.getMarketCap(self.extract_form_four_field(soup.find('issuer').find('issuerTradingSymbol')))),
             "rptOwnerCik" : self.extract_form_four_field(soup.find('rptOwnerCik'), default=""),
             "rptOwnerName" : self.extract_form_four_field(soup.find('rptOwnerName'), default=""),
-            "isDirector" : self.extract_form_four_field(soup.find('isDirector'), default=""),
-            "isOfficer" : self.extract_form_four_field(soup.find('isOfficer'), default=""),
-            "isTenPercentOwner" : self.extract_form_four_field(soup.find('isTenPercentOwner'), default=""),
-            "isOther" : self.extract_form_four_field(soup.find('isOther'), default=""),
+            "isDirector" : self.extract_form_four_field(soup.find('isDirector'), transform=lambda x: x.lower() == 'true', default=False),
+            "isOfficer" : self.extract_form_four_field(soup.find('isOfficer'), transform=lambda x: x.lower() == 'true', default=False),
+            "isTenPercentOwner" : self.extract_form_four_field(soup.find('isTenPercentOwner'), transform=lambda x: x.lower() == 'true', default=False),
+            "isOther" : self.extract_form_four_field(soup.find('isOther'), transform=lambda x: x.lower() == 'true', default=False),
             "officerTitle" : self.extract_form_four_field(soup.find('officerTitle'), default=""),
             "otherTitle" : self.extract_form_four_field(soup.find('otherTitle'), default=""),
             "nonDerivativeTable": non_derivative_table_dict,
