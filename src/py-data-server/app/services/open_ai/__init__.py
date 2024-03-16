e93ca94130f7be36116eead9c454d230cd89bd9a
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-03-16 14:41:57 -0400

    write competitor article

--- src/py-data-server/app/services/open_ai/__init__.py
+++ src/py-data-server/app/services/open_ai/__init__.py
@@ -19,3 +19,17 @@ class OpenAiService:
         print(completion.choices[0].message)    
     
     
+    async def write_competitor_article(competitor_article):
+        
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a high level financial journalist, skilled in explaining financial topics happening in the news better than anyone else."},
+            {"role": "user", "content": f"I am your boss. Our competitor wrote a news article before us and we are covering the exact same topic \
+                    Write a better article than this {competitor_article}   \
+                "}
+        ]
+        )
+        print(completion.choices[0].message)    
+        return completion
+    
--- src/py-data-server/app/services/open_ai/routes/__init__.py
+++ src/py-data-server/app/services/open_ai/routes/__init__.py
@@ -14,3 +14,13 @@ class OpenAiController:
     async def test_open_ai():
         data = await OpenAiService.test_open_ai()
         return data
+    
+    
+    # Write Competitor Article
+    @router.get("/write_competitor_article/")
+    async def write_competitor_article():
+        
+        test_article = 'Adobe Systems (NASDAQ:ADBE) shares dropped more than 14% intra-day today after it released guidance for the current quarter that fell short of expectations regarding a crucial revenue metric, sparking concerns over increasing competition affecting its growth trajectory. For the fiscal second quarter, Adobe has set a target for digital media net new revenue, at $440 million, which is below the approximately $460 million expected by analysts. The company has forecasted adjusted earnings per share (EPS) to be between $4.35 and $4.40, with revenue projections ranging from $5.25 billion to $5.30 billion. These figures compare less favorably with analyst predictions of $4.39 in EPS and $5.31 billion in revenue. This subdued forecast comes despite Adobe posting fiscal first-quarter results that exceeded analyst expectations, with an EPS of $4.48 on revenue of $5.18 billion, against forecasts of $4.38 in EPS on $5.14 billion in revenue.'
+        
+        data = await OpenAiService.write_competitor_article(test_article)
+        return data
